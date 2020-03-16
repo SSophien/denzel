6a06b70d62b72b761a5db337b9bb133990c5b579
SAM Sophien <sophien.sam@edu.devinci.fr>
2020-03-16 15:50:57 +0100

    API REST, (GET & PUT)

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 const {PORT} = require('./constants');
+const queryString = require('query-string');
+
+const db = require("./db");
+const dbName = "data";
+const collectionName = "movies";
+
+const DENZEL_IMDB_ID = "nm0000243";
 
 const app = express();
 
@@ -16,6 +23,73 @@ app.options('*', cors());
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
+
+db.initialize(dbName, collectionName, function(dbCollection) {
+	app.get('/movies/populate/' + DENZEL_IMDB_ID, (request, response) => {
+		// return full list
+		dbCollection.find().toArray((error, result) => {
+			if (error) throw error;
+			response.json(result);
+		});
+	});
+	
+	app.get('/movies', (request, response) => {
+		// return random must-watch movie
+		var query = {"metascore":{$gte:70}};
+		
+		dbCollection.aggregate([
+			{ $match: query },
+			{ $sample: {size: 1}}
+		]).toArray((error, result) => {
+			if (error) throw error;
+			response.json(result);
+		});
+	});	
+		
+	app.get('/movies/search', (request, response) => {
+		//get the id from params & return the movie corresponding
+		let limit = parseInt(request.query.limit, 10);
+		let metascore = parseInt(request.query.metascore, 10);
+		limit = limit?limit:5;
+		metascore = metascore?metascore:0;
+		
+		var query = {"metascore":{$gte:metascore}};
+		dbCollection.find(query).sort({metascore: -1}).limit(limit).toArray((error, result) => {
+			if (error) throw error;
+			response.json(result);
+		});
+	});
+	
+	app.put('/movies/:id', (request, response) => {
+		const itemId = request.params.id;
+		const item = request.body;
+		console.log("Editing item: ", itemId, " to be ", item);
+		
+		dbCollection.updateOne({id: itemId}, {$set: item}, (error, result) => {
+			if (error) throw error;
+			dbCollection.find().toArray(function(_error, _result) {
+				if(_error) throw _error;
+				response.json(_result);
+			});
+		});
+	});
+	
+	app.get('/movies/:id', (request, response) => {
+		//get the id from params & return the movie corresponding
+		const itemId = request.params.id;
+		dbCollection.findOne({id: itemId}, (error, result) => {
+			if (error) throw error;
+			response.json(result);
+		});
+	});
+
+
+}, function(err) {
+	throw (err);
+});
+
+
+
 
 app.listen(PORT);
 console.log(`📡 Running on port ${PORT}`);
